905d4a137c922834f6a851783a9ea64dd0ce4cca
Bogdan_Uni <bogdan.job10@gmail.com>
2024-10-21 18:25:54 +0100

    Creating Input - Activity 1 + BMI

--- Week1/input_task.py
+++ Week1/input_task.py
@@ -1,3 +1,20 @@
 print("Please enter your name")
 name = input()
 print(f"your name is {name}")
+
+
+print("Please enter your age")
+age = int(input())
+print(f"your age is {age}")
+
+print("Please enter your weight")
+weight = float(input())
+print(f"your weight is {weight}")
+
+print("Please enter your height")
+height = float(input())
+print(f"your height is {height}")
+
+bmi = weight/(height**2)
+
+print(f"Your BMI is {bmi}")
