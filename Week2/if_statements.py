54a8ff7a2deafcb9f8af82ce15832c65b6d942f2
Bogdan_Uni <bogdan.job10@gmail.com>
2024-10-21 19:05:59 +0100

    Week 2 - First task

---
+++ Week2/if_statements.py
@@ -0,0 +1,11 @@
+print("What type of book do you like")
+
+#Using input() Keyword to take the user input
+book = input()
+
+if book == "adventure":
+    print("I like adventure books!")
+else:
+    print("Wrong input by User")
+
+print("Finished reading the book.")
