fa10e4d1818a58f9ec859377755491aadec13278
BogdanIUNI <bogdan.job@Gmail.com>
2024-11-04 18:02:48 +0000

    Week 3 - nested loops 2

--- Week3/nesting.py
+++ Week3/nesting.py
@@ -4,11 +4,16 @@ sequence = input()
 print("Enter your character used in the sequence above")
 marker = input()
 
-dashes = "-"
+mposition1 = -1
+mposition2 = -1
 
-for i in sequence:
-    count = sequence.count(marker)
+for i in range (0, len(sequence), 1):
+    letter = sequence[i]
 
-    if i == marker:
-        print(f"The distance between the markers is {sequence.count(dashes, len(marker))}")
-        break
+    if letter == marker:
+        if(mposition1 == -1):
+            mposition1 = i
+        else:
+            mposition2 = i
+
+print(f"The distance between the markers is {mposition2 - mposition1}.")
