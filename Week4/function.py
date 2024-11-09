10000936e5907249fae166772e3ea9ed998e6057
BogdanIUNI <bogdan.job@Gmail.com>
2024-11-09 13:03:10 +0000

    Week 4 - Built in functions tasks

---
+++ Week4/function.py
@@ -0,0 +1,11 @@
+def number():
+    count = 0
+    print("Please enter a number:")
+    user_number = int(input())
+    count += user_number
+    print("Please enter another number:")
+    user_number2 = int(input())
+    count = user_number + user_number2
+    print(f"Your number is {count}")
+
+number()
---
+++ Week4/nesting_function.py
@@ -0,0 +1,9 @@
+def identify():
+    print("Enter a word representing what you see:")
+    word = input()
+    if word == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+identify()
